ac7692b9d767b8488cca1f153ecdaa6bef3ac684
Pysio <qq593277393@outlook.com>
2024-06-18 00:06:58 +0800

    Test Commit

--- src/main/java/cn/mcyou/McyouyouMod.java
+++ src/main/java/cn/mcyou/McyouyouMod.java
@@ -17,6 +17,6 @@ public class McyouyouMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("McYouYou Mod Install");
 	}
 }
